7f905630f693a6305c14bce83a47b709ba400856
Lukas Singer <7627101@isd535.org>
2022-04-18 10:31:05 -0500

    Fixed incorrect data pushing to db

--- scripts/Checkout.js
+++ scripts/Checkout.js
@@ -11,9 +11,11 @@ export class Checkout {
     Checkout.checkoutModalElement = document.querySelector(
       Checkout.CHECKOUT_MODAL_SELECTOR
     );
-    Checkout.checkoutModalElement.addEventListener("click", (e) => {
-      Checkout.checkOut();
-    });
+    document
+      .querySelector("[data-checkout-confirm]")
+      .addEventListener("click", (e) => {
+        Checkout.checkOut();
+      });
   }
 
   static openCheckoutModal() {
@@ -27,14 +29,14 @@ export class Checkout {
   static checkOut() {
     let order = {};
     order.order = Cart.order;
-    order.name = document.querySelector("data-checkout-name");
-    order.phone = document.querySelector("data-checkout-phone");
-    order.address = document.querySelector("data-checkout-address");
-    order.state = document.querySelector("data-checkout-state");
-    order.zip = document.querySelector("data-checkout-zip");
-    order.ccn = document.querySelector("data-checkout-cc-n");
-    order.cce = document.querySelector("data-checkout-cc-e");
-    order.ccc = document.querySelector("data-checkout-cc-c");
+    order.name = document.querySelector("[data-checkout-name]").value;
+    order.phone = document.querySelector("[data-checkout-phone]").value;
+    order.address = document.querySelector("[data-checkout-address]").value;
+    order.state = document.querySelector("[data-checkout-state]").value;
+    order.zip = document.querySelector("[data-checkout-zip]").value;
+    order.ccn = document.querySelector("[data-checkout-cc-n]").value;
+    order.cce = document.querySelector("[data-checkout-cc-e]").value;
+    order.ccc = document.querySelector("[data-checkout-cc-c]").value;
     let orders = DataStore.get("orders");
     orders.push(order);
     DataStore.set("orders", orders);
